010e1b89ea15b9818e1267f2d443a6bc8211d808
Meg Galvez <meg@oakmaine.com>
2019-05-22 03:39:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,11 @@
 require 'pry'
 
 class Artist
-  extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable
   include Paramable
+ 
   attr_accessor :name
   attr_reader :songs
 
